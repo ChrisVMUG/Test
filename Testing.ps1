6d9c22d9fc0a5930c7b9d2621c641f571a0766ab
Chris Hildebrandt <VMUG@childebrandt42.com>
2023-05-19 08:37:28 -0700

    Added App and Appx

--- Testing.ps1
+++ Testing.ps1
@@ -1,3 +1,20 @@
 Write-host "Its VMUG"
 
 Write-host "Toaster is a Jar"
+
+$InstalledApps = Get-WmiObject -Class win32_product
+
+ForEach ($App in $InstalledApps){
+    Write-host $App.Name
+
+}
+
+$AppxPackages = Get-AppxPackage -AllUsers | Select Name,version,Packagefullname
+
+Foreach ($Package in $AppxPackages){
+    Write-host $Pacage.name
+    if ($Package.name -like "*Microsoft*") {
+        Write-host "This is probally bloatware! "
+    }
+}
+
